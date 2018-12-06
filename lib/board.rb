55c18523b81fd50cb9ec54ea26316acb8d3b2bf2
Peregrine Balas <peregrinereedbalas@gmail.com>
2018-12-05 19:07:13 -0700

    We did it! We validated placements

--- lib/board.rb
+++ lib/board.rb
@@ -31,11 +31,11 @@ class Board
   def row_confirm(y_axis)
 
     valid_placements = []
-    row_index = y_axis[0][0]
+    row_sample = y_axis[0][0]
     cell_keys = cells.keys
 
     cell_keys.each_cons(y_axis.length) do |cell_key|
-      if cell_key.all? {|key| key[0].eql?(row_index)}
+      if cell_key.all? {|key| key[0].eql?(row_sample)}
         valid_placements << cell_key
       end
     end
@@ -45,11 +45,27 @@ class Board
     end
   end
 
+  def column_confirm(x_axis)
+
+    cell_keys = cells.keys
+    column_sample = x_axis[0][1]
+    coordinate_pairs = []
+
+    x_axis.each_cons(2) do |coordinate|
+      coordinate_pairs << coordinate
+    end
+
+    coordinate_pairs.all? do |pair|
+      pair[1][0].ord - pair[0][0].ord == 1 && pair[0][1] == pair[1][1]
+    end
+
+  end
+
   def valid_placement?(ship, coordinates)
-    if coordinates.count == ship.length && ship.class == Ship
-      if row_confirm(coordinates)
+    confirm_ship = coordinates.count == ship.length && ship.class == Ship
+    confirm_coordinates = column_confirm(coordinates) || row_confirm(coordinates)
+    if confirm_ship && confirm_coordinates
         true
-      end
     else
       false
     end
